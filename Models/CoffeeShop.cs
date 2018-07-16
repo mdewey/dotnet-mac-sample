39e7049da798615ff33eebef17c7a2ae6cef41d1
Mark Dewey <mark.t.dewey@gmail.com>
2018-07-16 14:13:43 -0400

    found direction

---
+++ Models/CoffeeShop.cs
@@ -0,0 +1,13 @@
+public class CoffeeShop
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public int Price { get; set; }
+    public int Rating { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
+
+
+//https://docs.microsoft.com/en-us/aspnet/core/tutorials/web-api-vsc?view=aspnetcore-2.1
